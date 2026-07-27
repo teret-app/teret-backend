ce5410820cb065f4b2fa81b853f38b45cc8af7cd
Mladen Petrovic <gangesterpet@gmail.com>
2026-07-27 10:09:02 +0200

    Fix forgot password route

--- index.js
+++ index.js
@@ -1427,7 +1427,7 @@ app.post('/login', async (req, res) => {
     });
   }
 });
-    const
+
 app.post('/forgot-password', async (req, res) => {
   try {
     const email = normalizeString(req.body.email).toLowerCase();
