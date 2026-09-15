a25968b53402eea9f821a5f69c16b6a3541d4b09
Mladen Petrovic <gangesterpet@gmail.com>
2026-09-15 08:30:51 +0200

    Set latest app version to 15

--- index.js
+++ index.js
@@ -2460,7 +2460,7 @@ app.get('/me', authMiddleware, (req, res) => {
     phone: user.phone,
     role: user.role,
     isAdmin: user.isAdmin === true,
-    latestAppVersionCode: 14,
+    latestAppVersionCode: 15,
     emailVerified: user.emailVerified === true,
     reliabilityMisses: Number(user.reliabilityMisses || 0),
     senderNoSelectionCount: Number(user.senderNoSelectionCount || 0),
